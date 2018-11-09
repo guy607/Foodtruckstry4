05143568fb0a83af78586ee746ab500167789398
Jacob <jcthomas556@byui.com>
2018-11-09 13:39:08 -0700

    trying to get it to show up

--- app/src/main/java/com/jeff/murray/foodtruckstry4/TestActivity1.java
+++ app/src/main/java/com/jeff/murray/foodtruckstry4/TestActivity1.java
@@ -3,12 +3,15 @@ package com.jeff.murray.foodtruckstry4;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class TestActivity1 extends AppCompatActivity {
+public class TestActivity1 extends AppCompatActivity {//sandwiches activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sandwich_select);
     }
+
+
+
 
 }
